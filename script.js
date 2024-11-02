818f94aa2d80bb7f941400e807fd5caf18385509
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-11-03 00:52:40 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-var myName = document.querySelector('.hero-text h1')
-var myRole = document
+const logoClick = document.querySelector('.logo')
+const clickOff = document.querySelector('.click-me')
+const textElements = document.querySelectorAll('.to-appear');
+logoClick.addEventListener('click',()=>{
+    clickOff.style.display = 'none'
+    textElements.forEach((el, index) => {
+        setTimeout(() => {
+            el.style.opacity = 1;
+        }, index * 200); // Delay each element by 500ms
+    });
+})
+
+const lamp = document.getElementById('lamp');
+const brightnessControl = document.getElementById('brightness');
+
+brightnessControl.addEventListener('input', () => {
+    const brightness = brightnessControl.value;
+    // Increase the size and intensity of the glow as brightness increases
+    lamp.style.boxShadow = `0 0 ${brightness}px ${brightness / 2}px rgba(255, 215, 0, ${brightness / 100})`;
+})
+
+
+
+
