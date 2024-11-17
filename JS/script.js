105a11afe7264adc38866485a4f9d0513a5f0d1d
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-11-17 22:00:27 +0530

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,17 @@
-const logoClick = document.querySelector('.logo')
-const clickOff = document.querySelector('.click-me')
-const main = document.querySelector('.main')
-const textElements = document.querySelectorAll('.to-appear')
-logoClick.addEventListener('click',()=>{
-    clickOff.style.display = 'none'
-    main.style.overflowX = 'visible';
+window.addEventListener('load', () => {
+    const textElements = document.querySelectorAll('.to-appear');
+    const bgImage = document.querySelector('main')
     textElements.forEach((el, index) => {
         setTimeout(() => {
             el.style.opacity = 1;
+            bgImage.classList.add('main');
         }, index * 200); // Delay each element by 500ms
     });
 })
 
-const lamp = document.getElementById('lamp');
-const brightnessControl = document.getElementById('brightness');
-
 brightnessControl.addEventListener('input', () => {
+    const lamp = document.getElementById('lamp');
+    const brightnessControl = document.getElementById('brightness');
     const brightness = brightnessControl.value;
     // Increase the size and intensity of the glow as brightness increases
     lamp.style.boxShadow = `0 0 ${brightness}px ${brightness / 2}px rgba(255, 215, 0, ${brightness / 100})`;
