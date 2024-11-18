caa5cd698101c3460031a8a607d16676c45c5398
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-11-19 00:00:55 +0530

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -9,14 +9,37 @@ window.addEventListener('load', () => {
     });
 })
 
-brightnessControl.addEventListener('input', () => {
-    const lamp = document.getElementById('lamp');
-    const brightnessControl = document.getElementById('brightness');
-    const brightness = brightnessControl.value;
-    // Increase the size and intensity of the glow as brightness increases
-    lamp.style.boxShadow = `0 0 ${brightness}px ${brightness / 2}px rgba(255, 215, 0, ${brightness / 100})`;
+function rotateImage() {
+    const imgCnt = document.querySelector('.img-cnt');
+    let left = 0;
+    setInterval(() => {
+        imgCnt.style.marginLeft = left + 'px';
+        if (left === -240) {
+            left = 0;
+        }else{
+            left += -120;
+        }
+    }, 2000)
+}
+rotateImage();
+
+document.addEventListener('click', (e) => {
+    if (e.target.className === 'about-me to-appear') {
+        document.querySelector('.about-wrapper').classList.toggle('open');
+    }
+    console.log(e);
+    if (e.target.className === 'hero-page' || e.target.className === 'header') {
+        document.querySelector('.about-wrapper').classList.remove('open');
+    }
 })
 
+// const brightnessControl = document.getElementById('brightness');
+// brightnessControl.addEventListener('input', (e) => {
+//     const lamp = document.getElementById('lamp');
+//     const brightness = e.target.value;
+//     // Increase the size and intensity of the glow as brightnes s increases
+//     lamp.style.boxShadow = `0 0 ${brightness}px ${brightness / 2}px rgba(255, 215, 0, ${brightness / 100})`;
+// })
 
 
 
